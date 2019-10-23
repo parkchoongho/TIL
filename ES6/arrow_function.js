4dc881cf03af10bb089f887094a0b0a0f657184d
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-10-23 15:00:21 +0900

    add arrow_function.js

---
+++ ES6/arrow_function.js
@@ -0,0 +1,22 @@
+// ES5
+function add(a, b) {
+  return a + b;
+}
+
+// ES6
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const mul = (a, b) => a * b;
+
+const square = a => a * a;
+
+const logging = () => console.log("logging....");
+
+const obj = {
+  name: "Choong Ho",
+  sayHello: () => {
+    console.log("Hello!!");
+  }
+};
