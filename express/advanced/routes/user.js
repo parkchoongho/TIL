3f34b50625f4ce1e08bc94b93ac5c8d5b2cdb39e
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-11-07 11:16:25 +0900

    add using user model

--- express/advanced/routes/user.js
+++ express/advanced/routes/user.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const { User, validateUser } = require("../models/user");
 const router = express.Router();
 
-const users = [
-  { id: 1, name: "park" },
-  { id: 2, name: "choi" },
-  { id: 3, name: "kong" }
-];
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const users = await User.find();
   res.send(users);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.send(user);
+});
+
+router.post("/", async (req, res) => {
+  if (validateUser(req.body).error) {
+    res.status(400).send("잘못된 요청입니다.");
+    return;
+  }
+  const user = new User({
+    name: req.body.name
+  });
+  const result = await user.save();
+  res.status(200).send(result);
 });
 
 module.exports = router;
