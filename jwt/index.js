bce6930f7f419f8306400a8f852ffad0c5e177ef
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-11-11 11:42:08 +0900

    add index.js

---
+++ jwt/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const users = require("./users");
+const verifyToken = require("./verifyToken");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find(u => u.email === email && u.password === password);
+  if (!user) {
+    res.json({ error: "check your email or password" });
+    return;
+  }
+  const secretKey = process.env.TOKEN_KEY || "secret";
+  const token = jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    },
+    secretKey,
+    { expiresIn: "1m" }
+  );
+  res.json({ jwt: token });
+});
+
+app.get("/personal", (req, res) => {
+  const { data, error } = verifyToken(req);
+  if (data) res.json(data);
+  if (error) res.json(error);
+});
+
+app.listen(3000, () => console.log("서버 가동중"));
